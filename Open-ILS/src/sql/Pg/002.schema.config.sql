28293dd4af35ffcf36321e5ebec2d01be0957cbb
blake <blake@mobiusconsortium.org>
2026-08-24 17:02:09 -0500

    LP#2144042: Stamp Upgrade Script
    Signed-off-by: blake <blake@mobiusconsortium.org>

--- Open-ILS/src/sql/Pg/002.schema.config.sql
+++ Open-ILS/src/sql/Pg/002.schema.config.sql
@@ -92,7 +92,7 @@ CREATE TRIGGER no_overlapping_deps
     BEFORE INSERT OR UPDATE ON config.db_patch_dependencies
     FOR EACH ROW EXECUTE PROCEDURE evergreen.array_overlap_check ('deprecates');
 
-INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1525', :eg_version); -- stompro/smorrison/sandbergja
+INSERT INTO config.upgrade_log (version, applied_to) VALUES ('1526', :eg_version); -- ianskelskey/Bmagic
 
 CREATE TABLE config.bib_source (
 	id		SERIAL	PRIMARY KEY,
