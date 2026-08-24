ac47aa5445a33488bd597f7927c29f8555584a94
blake <blake@mobiusconsortium.org>
2026-08-24 16:42:34 -0500

    LP#2146576: stamp upgrade script Retro-fixing
    Fixing a commit mistake, bringing 1525 db upgrade script
    back from the dead.
    
    Signed-off-by: blake <blake@mobiusconsortium.org>

--- Open-ILS/src/sql/Pg/version-upgrade/3.17.2-3.17.3-upgrade-db.sql
+++ Open-ILS/src/sql/Pg/version-upgrade/3.17.2-3.17.3-upgrade-db.sql
@@ -2,6 +2,16 @@
 \set eg_version '''3.17.3'''
 BEGIN;
 INSERT INTO config.upgrade_log (version, applied_to) VALUES ('3.17.3', :eg_version);
+
+SELECT evergreen.upgrade_deps_block_check('1525', :eg_version);
+
+UPDATE config.org_unit_setting_type
+SET grp='holds'
+WHERE
+name='circ.holds.calculated_age_proximity'
+and grp='circ'
+;
+
 COMMIT;
 
 -- Update auditor tables to catch changes to source tables.
